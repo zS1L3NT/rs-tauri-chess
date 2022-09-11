a37cb527c28bbdb1939492e6e8745f7e33a8b130
Zechariah <zechariahtan144@gmail.com>
2022-09-11 14:49:46 +0800

    ✨ temporarily remove tauri from project

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,18 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+use engine::board::Board;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+mod engine;
+// #![cfg_attr(
+//     all(not(debug_assertions), target_os = "windows"),
+//     windows_subsystem = "windows"
+// )]
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    String::from("Hello from Rust!")
-}
+fn get_possible_moves() {}
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    Board::new();
+    // tauri::Builder::default()
+    //     .invoke_handler(tauri::generate_handler![])
+    //     .run(tauri::generate_context!())
+    //     .expect("error while running tauri application");
 }
