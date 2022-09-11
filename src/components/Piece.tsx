b0f4157a2f2cbfddff67cf73a9ea331871081054
Zechariah <zechariahtan144@gmail.com>
2022-09-11 10:51:16 +0800

    ✨ ignore middle and right click

--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -32,6 +32,8 @@ const Piece = ({ piece: { id, color, type, square } }: { piece: iPiece }) => {
 	}, [ref.current, square])
 
 	const handleMouseDown = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+		if (e.button !== 0) return
+
 		// Piece is already selected, track what happens to it
 		if (selected?.id === id) {
 			setMoved(false)
@@ -43,40 +45,42 @@ const Piece = ({ piece: { id, color, type, square } }: { piece: iPiece }) => {
 	}
 
 	const handleMouseMove = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
-		if (isDragging) {
-			// If it's moved, it should stay selected
-			if (moved !== null) {
-				setMoved(true)
-			}
+		if (e.button !== 0) return
+		if (!isDragging) return
 
-			setHovered(calculateSquareFromCss(e.currentTarget.style))
+		// If it's moved, it should stay selected
+		if (moved !== null) {
+			setMoved(true)
 		}
+
+		setHovered(calculateSquareFromCss(e.currentTarget.style))
 	}
 
 	const handleMouseUp = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
-		if (selected?.id === id) {
-			setIsDragging(false)
-			setHovered(null)
-			setMoved(null)
+		if (e.button !== 0) return
+		if (selected?.id !== id) return
 
-			// If it didn't move, de-select it
-			if (moved === false) {
-				setSelected(null)
-				return
-			}
+		setIsDragging(false)
+		setHovered(null)
+		setMoved(null)
 
-			// ! Validate before finalizing the move
-			const targetSquare = calculateSquareFromCss(e.currentTarget.style)
-			const targetPiece = pieces.find(p => equalSquares(p.square, targetSquare))
-			if (targetPiece && targetPiece.id !== id) {
-				setPieces(
-					pieces
-						.map(p => (p.id === id ? { ...p, square: targetSquare } : p))
-						.filter(p => p.id !== targetPiece.id)
-				)
-			} else {
-				setPieces(pieces.map(p => (p.id === id ? { ...p, square: targetSquare } : p)))
-			}
+		// If it didn't move, de-select it
+		if (moved === false) {
+			setSelected(null)
+			return
+		}
+
+		// ! Validate before finalizing the move
+		const targetSquare = calculateSquareFromCss(e.currentTarget.style)
+		const targetPiece = pieces.find(p => equalSquares(p.square, targetSquare))
+		if (targetPiece && targetPiece.id !== id) {
+			setPieces(
+				pieces
+					.map(p => (p.id === id ? { ...p, square: targetSquare } : p))
+					.filter(p => p.id !== targetPiece.id)
+			)
+		} else {
+			setPieces(pieces.map(p => (p.id === id ? { ...p, square: targetSquare } : p)))
 		}
 	}
 
