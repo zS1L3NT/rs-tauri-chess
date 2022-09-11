a6923d46f7961bcb10114909d4f1dcb205d4ebd9
Zechariah <zechariahtan144@gmail.com>
2022-09-11 10:00:34 +0800

    ✨ default board config

---
+++ src/defaultBoard.ts
@@ -0,0 +1,292 @@
+import { Color, File, Piece, Rank, Type } from "./types"
+
+export default <Piece[]>[
+	{
+		id: 0,
+		type: Type.Rook,
+		color: Color.Black,
+		square: {
+			file: File.A,
+			rank: Rank._8
+		}
+	},
+	{
+		id: 1,
+		type: Type.Knight,
+		color: Color.Black,
+		square: {
+			file: File.B,
+			rank: Rank._8
+		}
+	},
+	{
+		id: 2,
+		type: Type.Bishop,
+		color: Color.Black,
+		square: {
+			file: File.C,
+			rank: Rank._8
+		}
+	},
+	{
+		id: 3,
+		type: Type.Queen,
+		color: Color.Black,
+		square: {
+			file: File.D,
+			rank: Rank._8
+		}
+	},
+	{
+		id: 4,
+		type: Type.King,
+		color: Color.Black,
+		square: {
+			file: File.E,
+			rank: Rank._8
+		}
+	},
+	{
+		id: 5,
+		type: Type.Bishop,
+		color: Color.Black,
+		square: {
+			file: File.F,
+			rank: Rank._8
+		}
+	},
+	{
+		id: 6,
+		type: Type.Knight,
+		color: Color.Black,
+		square: {
+			file: File.G,
+			rank: Rank._8
+		}
+	},
+	{
+		id: 7,
+		type: Type.Rook,
+		color: Color.Black,
+		square: {
+			file: File.H,
+			rank: Rank._8
+		}
+	},
+	{
+		id: 8,
+		type: Type.Pawn,
+		color: Color.Black,
+		square: {
+			file: File.A,
+			rank: Rank._7
+		}
+	},
+	{
+		id: 9,
+		type: Type.Pawn,
+		color: Color.Black,
+		square: {
+			file: File.B,
+			rank: Rank._7
+		}
+	},
+	{
+		id: 10,
+		type: Type.Pawn,
+		color: Color.Black,
+		square: {
+			file: File.C,
+			rank: Rank._7
+		}
+	},
+	{
+		id: 11,
+		type: Type.Pawn,
+		color: Color.Black,
+		square: {
+			file: File.D,
+			rank: Rank._7
+		}
+	},
+	{
+		id: 12,
+		type: Type.Pawn,
+		color: Color.Black,
+		square: {
+			file: File.E,
+			rank: Rank._7
+		}
+	},
+	{
+		id: 13,
+		type: Type.Pawn,
+		color: Color.Black,
+		square: {
+			file: File.F,
+			rank: Rank._7
+		}
+	},
+	{
+		id: 14,
+		type: Type.Pawn,
+		color: Color.Black,
+		square: {
+			file: File.G,
+			rank: Rank._7
+		}
+	},
+	{
+		id: 15,
+		type: Type.Pawn,
+		color: Color.Black,
+		square: {
+			file: File.H,
+			rank: Rank._7
+		}
+	},
+	{
+		id: 16,
+		type: Type.Pawn,
+		color: Color.White,
+		square: {
+			file: File.A,
+			rank: Rank._2
+		}
+	},
+	{
+		id: 17,
+		type: Type.Pawn,
+		color: Color.White,
+		square: {
+			file: File.B,
+			rank: Rank._2
+		}
+	},
+	{
+		id: 18,
+		type: Type.Pawn,
+		color: Color.White,
+		square: {
+			file: File.C,
+			rank: Rank._2
+		}
+	},
+	{
+		id: 19,
+		type: Type.Pawn,
+		color: Color.White,
+		square: {
+			file: File.D,
+			rank: Rank._2
+		}
+	},
+	{
+		id: 20,
+		type: Type.Pawn,
+		color: Color.White,
+		square: {
+			file: File.E,
+			rank: Rank._2
+		}
+	},
+	{
+		id: 21,
+		type: Type.Pawn,
+		color: Color.White,
+		square: {
+			file: File.F,
+			rank: Rank._2
+		}
+	},
+	{
+		id: 22,
+		type: Type.Pawn,
+		color: Color.White,
+		square: {
+			file: File.G,
+			rank: Rank._2
+		}
+	},
+	{
+		id: 23,
+		type: Type.Pawn,
+		color: Color.White,
+		square: {
+			file: File.H,
+			rank: Rank._2
+		}
+	},
+	{
+		id: 24,
+		type: Type.Rook,
+		color: Color.White,
+		square: {
+			file: File.A,
+			rank: Rank._1
+		}
+	},
+	{
+		id: 25,
+		type: Type.Knight,
+		color: Color.White,
+		square: {
+			file: File.B,
+			rank: Rank._1
+		}
+	},
+	{
+		id: 26,
+		type: Type.Bishop,
+		color: Color.White,
+		square: {
+			file: File.C,
+			rank: Rank._1
+		}
+	},
+	{
+		id: 27,
+		type: Type.Queen,
+		color: Color.White,
+		square: {
+			file: File.D,
+			rank: Rank._1
+		}
+	},
+	{
+		id: 28,
+		type: Type.King,
+		color: Color.White,
+		square: {
+			file: File.E,
+			rank: Rank._1
+		}
+	},
+	{
+		id: 29,
+		type: Type.Bishop,
+		color: Color.White,
+		square: {
+			file: File.F,
+			rank: Rank._1
+		}
+	},
+	{
+		id: 30,
+		type: Type.Knight,
+		color: Color.White,
+		square: {
+			file: File.G,
+			rank: Rank._1
+		}
+	},
+	{
+		id: 31,
+		type: Type.Rook,
+		color: Color.White,
+		square: {
+			file: File.H,
+			rank: Rank._1
+		}
+	}
+]
